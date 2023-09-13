8511571a1b3ecb95a461f9ab991d83dea8fdb1b8
0russus0 <pierreetleroux@gmail.com>
2023-09-13 11:55:34 +0200

    Start Step 2

--- 02 - events-management/js/exercices.js
+++ 02 - events-management/js/exercices.js
@@ -1,1 +1,16 @@
-// Your code goes here
+// Exercices JavaScript : Gestion des Événements
+//Exercice 1 : Cliquer sur un Bouton
+
+let myButton = document.createElement("a");
+myButton.innerText = "Tadaaaa !";
+myButton.href = "https://www.google.fr/";
+myButton.classList.add( "btn", "btn-primary", "my-button");
+myButton.setAttribute("disabled", "true");
+document.querySelector("#clickme").appendChild(myButton);
+
+
+myButton.addEventListener("click", function(e){
+    e.preventDefault(); 
+    let newPicture = document.getElementById("pictureTarget");
+    newPicture.src = "images/Musclor.jpg";
+});
