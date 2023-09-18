d9566a73f5fda7f4cf0cf76989497aa7650501b2
0russus0 <pierreetleroux@gmail.com>
2023-09-18 22:13:49 +0200

    commit for ending Step 2

--- 02 - events-management/js/exercices.js
+++ 02 - events-management/js/exercices.js
@@ -2,15 +2,74 @@
 //Exercice 1 : Cliquer sur un Bouton
 
 let myButton = document.createElement("a");
-myButton.innerText = "Tadaaaa !";
-myButton.href = "https://www.google.fr/";
-myButton.classList.add( "btn", "btn-primary", "my-button");
+myButton.innerText = "Visiter Musclor.org";
+myButton.href = "http://www.musclor.org/";
+myButton.target = "_blank";
+myButton.classList.add("btn", "btn-primary", "my-button");
 myButton.setAttribute("disabled", "true");
 document.querySelector("#clickme").appendChild(myButton);
 
+myButton.addEventListener("click", function (e) {
+    window.alert("Par le pouvoir du crane ancestral!!!!!");
+});
 
-myButton.addEventListener("click", function(e){
-    e.preventDefault(); 
+//Exercice 2 : Survoler un Élément
+let myButton2 = document.createElement("a");
+myButton2.innerText = "Tadaa!!!";
+myButton2.href = "#";
+myButton2.classList.add("btn", "btn-primary", "my-button");
+myButton.setAttribute("disabled", "true");
+document.querySelector("#hoverme").appendChild(myButton2);
+
+myButton2.addEventListener("mouseover", function (e) {
+    e.preventDefault();
     let newPicture = document.getElementById("pictureTarget");
     newPicture.src = "images/Musclor.jpg";
 });
+
+//Exercice 3 : Survoler un Élément
+document.addEventListener("keydown", function () {
+    let input = document.getElementById("unicInput").value;
+    console.log(input);
+});
+
+//Exercice 4 : Cliquer en Dehors d'un Élément
+document.addEventListener("click", function (e) {
+    let dialog = document.querySelector("#blabla");
+    if (!dialog.contains(e.target)) {
+        console.log(dialog);
+        dialog.style.display = "none";
+    }
+});
+
+//Exercice 5 : Cliquer en Dehors d'un Élément
+function valider() {
+    if (document.getElementById("prenom") == "") {
+        alert("Veuillez entrer votre prénom!");
+        return false;
+    }
+
+    if (document.getElementById("nom") == "") {
+        alert("Veuillez entrer votre nom!");
+        return false;
+    }
+
+    if (document.getElementById("mail") == "") {
+        alert("Veuillez entrer votre e-mail!");
+        return false;
+    }
+}
+
+//Exercice 6 : Défilement de la Page
+window.onscroll = function () {
+    myFunction();
+};
+
+function myFunction() {
+    if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 1300
+    ) {
+        document.getElementById("long").className = "toLong";
+    }
+}
