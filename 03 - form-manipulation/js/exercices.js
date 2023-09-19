a386d0c886d3e2af7122be7d9a604fd915587007
0russus0 <pierreetleroux@gmail.com>
2023-09-19 10:12:59 +0200

    Start Step 3

--- 03 - form-manipulation/js/exercices.js
+++ 03 - form-manipulation/js/exercices.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// Exercice 1 : Validation de Formulaire
+document.forms[0].addEventListener("submit", function (evenement) {
+    if (document.getElementById("name").value == "") {
+        evenement.preventDefault();
+        alert("Veuillez indiquer votre nom");
+        document.getElementById("email").focus();
+    } else if (document.getElementById("firstName").value == "") {
+        evenement.preventDefault();
+        alert("Veuillez indiquer votre prénom");
+        document.getElementById("email").focus();
+    }
+});
